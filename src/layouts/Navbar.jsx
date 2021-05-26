fbca5c5c3c4fe0938a243e417e072926bfae3683
Vincent Dizon <vince@kampo.ph>
2021-05-26 13:52:22 +0800

    added material ui and normalize.css

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {};
+
+export default Navbar;
